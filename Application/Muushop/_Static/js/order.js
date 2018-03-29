c78d7e27e607f429dcf756fa0366ea5ce6f8c34d
dameng <59262424@qq.com>
2018-03-29 09:44:52 +0800

    修复用户中心首页订单取消与确认收货按钮JS事件绑定

--- Application/Muushop/_Static/js/order.js
+++ Application/Muushop/_Static/js/order.js
@@ -1,27 +1,30 @@
 $(function(){
 	//取消订单
-	$('[data-toggle="cannel_order"]').click(function(){
-		var _this = $(this);
-		var data={
-			id : _this.data('id'),
-		};
-		$.post('/Muushop/api/cancel_order',data,function (ret) {
-	        //ret = JSON.parse(ret);
-		    if(ret.status==1){
-		        toast.success(ret.info, '温馨提示');
-		        setTimeout(function () {
-		            window.location.href = ret.url;
-		        }, 1000);
-		    }else{
-		        toast.error(ret.info, '温馨提示');
-		    }
-		})
-	})
+	$(document).on('click','[data-toggle="cannel_order"]',function(){
+		if(confirm("是否取消？")==true){
+			var _this = $(this);
+			var data={
+				id : _this.data('id'),
+			};
+			$.post('/Muushop/api/cancel_order',data,function (ret) {
+		        //ret = JSON.parse(ret);
+			    if(ret.status==1){
+			        toast.success(ret.info, '温馨提示');
+			        setTimeout(function () {
+			            window.location.href = ret.url;
+			        }, 1000);
+			    }else{
+			        toast.error(ret.info, '温馨提示');
+			    }
+			});
+		}
+	});
+
 });
 
 $(function(){
 	//确认收货
-	$('[data-toggle="do_receipt"]').click(function(){
+	$(document).on('click','[data-toggle="do_receipt"]',function(){
 		if(confirm("是否确认收货？")==true){
 			var _this = $(this);
 			var data={
